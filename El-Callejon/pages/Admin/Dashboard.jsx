b024062b4fff0b53d6ca3275a61b6ef6d15998ba
MRV-dev <a.marv0903@gmail.com>
2025-05-09 09:44:32 +0800

    Admin-Dashboard

--- El-Callejon/pages/Admin/Dashboard.jsx
+++ El-Callejon/pages/Admin/Dashboard.jsx
@@ -1,9 +1,16 @@
-
+import logoAdmin from '../assets/El_Calejon.jpg'
 
 function Dashboard() {
   return(
     <>
-      dashboard
+      <div className="Dashboard-page">
+        <div className="side-bar">
+          <img className="Logo-Admin" src={logoAdmin} />
+          <div className="navbar">
+            <h1 className='Dashboard-text'>DASHBOARD</h1>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
