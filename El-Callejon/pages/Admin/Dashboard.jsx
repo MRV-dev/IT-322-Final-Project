44c9bb01997c1eaef3a15bcebaf218c8c1520fa7
MRV-dev <a.marv0903@gmail.com>
2025-05-11 02:05:48 +0800

    bukas ko tuloy dashboard

--- El-Callejon/pages/Admin/Dashboard.jsx
+++ El-Callejon/pages/Admin/Dashboard.jsx
@@ -4,7 +4,7 @@ import logoAdmin from '../assets/El_Calejon.jpg'
 function Dashboard() {
   return(
     <>
-      <div className="Dashboard-page">
+      <div className="whole-page">
         <div className="side-bar">
           <img className="Logo-Admin" src={logoAdmin} />
           <ul className='List'>
@@ -14,29 +14,44 @@ function Dashboard() {
             <li className='navigations'>awdawd</li>
             <li className='navigations'>awda</li>
             <li className='navigations'>awdawd</li>
-            <hr className='line'/>
           </ul>
+          <hr className='line'/>
+        </div>
+
+        <div className='main-content-dashboard'>
           <div className="navbar">
             <h1 className='Dashboard-text'>DASHBOARD</h1>
           </div>
-          <div className='outer-box'>
-            <div className='notif-box'>
-              <Link to="/POS" className="Link-to-pos">
-                <div className='notif'>
-                  <span className='new-order-text'>New Orders:</span> You have 4 new online orders. Check the Orders section to prepare them!
+          <div className='overview-container'>
+            <div className='notif-position'>
+              <div className='outer-box'>
+                <div className='notif-box'>
+                  <Link to="/POS" className="Link-to-pos">
+                    <div className='notif'>
+                      <span className='new-order-text'>New Orders:</span> You have 4 new online orders. Check the Orders section to prepare them!
+                    </div>
+                  </Link> 
                 </div>
-              </Link> 
+              </div>
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
             
-          <div></div>
-          <div></div>
-          <div></div>
+            <div className='summary-cards'>
+              <div className='total-sales-today'>
+                <p className='total-sales-text'>Total Sales Today</p>
+                <h2 className='total-sales'>₱12,340</h2>
+              </div>
+              <div className='total-orders-today'>
+                <p className='total-orders-text'>Total Orders Today</p>
+                <h2 className='total-orders'>55</h2>
+              </div>
+              <div className='peak-sales-hour'>
+                <p className='peak-sales-text'>Peak Sales Hour</p>
+                <h2 className='peak-sales'>12PM</h2>
+              </div>
+            </div>
+          </div>
         </div>
+         
       </div>
     </>
   )
