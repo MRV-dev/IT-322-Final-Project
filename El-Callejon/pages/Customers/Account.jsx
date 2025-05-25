ed8c2604f452d83af9edaffc1281677937eaed19
MRV-dev <a.marv0903@gmail.com>
2025-05-25 14:48:07 +0800

    .

--- El-Callejon/pages/Customers/Account.jsx
+++ El-Callejon/pages/Customers/Account.jsx
@@ -1,7 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import Logo from '../assets/El_Calejon.jpg';
-import Profile from '../assets/Profile.jpg';
+import Profile from '../assets/profile.jpg';
 import '../Customers/customer-css/Account.css';
 import { FaBell, FaUser, FaLock, FaClock, FaClipboardList, FaSignOutAlt, FaCamera, FaTrash } from 'react-icons/fa';
 
